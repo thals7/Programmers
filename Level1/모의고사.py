cb7fe873239908756594b9316a0916badddeca60
thals7 <thals_7@naver.com>
2021-04-28 16:29:52 +0900

    solve 모의고사

---
+++ Level1/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    correct = [0, 0, 0]
+    answer = []
+
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    for i in range(len(answers)):
+        if answers[i] == first[i % 5]:
+            correct[0] += 1
+        if answers[i] == second[i % 8]:
+            correct[1] += 1
+        if answers[i] == third[i % 10]:
+            correct[2] += 1
+
+    for i in range(3):
+        if correct[i] == max(correct):
+            answer.append(i + 1)
+
+    return answer
