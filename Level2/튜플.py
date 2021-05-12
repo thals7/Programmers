7f1aac85f1735d89e4a2b59f9e9a2455ac93e85e
thals7 <thals_7@naver.com>
2021-05-12 22:29:52 +0900

    solve 튜플 (+정규 표현식 풀이 추가)

--- Level2/튜플.py
+++ Level2/튜플.py
@@ -1,4 +1,4 @@
-# 2019 카카오 개발자 겨울 인턴십
+# # # 2019 카카오 개발자 겨울 인턴십
 
 def solution(s):
     num = ""
@@ -25,3 +25,20 @@ def solution(s):
     answer = list(map(int,dic))
 
     return answer
+
+
+# 정규 표현식 이용한 풀이
+
+from collections import Counter
+import re
+
+def solution(s):
+
+    s = Counter(re.findall('\d+', s))
+    return list(map(int, sorted(s, key = lambda x:s[x], reverse = True)))
+
+# most_common() 함수 이용하여 가장 흔한 요소부터 적은 것 순으로 나열하는 리스트 반환
+def solution(s):
+
+    s = Counter(re.findall('\d+', s)).most_common()
+    return list(map(int, list(i[0] for i in s)))
