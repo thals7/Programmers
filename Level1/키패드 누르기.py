1068852a6aa48d696b712c76517b14b6a24e61c5
thals7 <thals_7@naver.com>
2021-05-13 11:57:09 +0900

    solve 키패드 누르기 (+풀이법 추가)

--- Level1/키패드 누르기.py
+++ Level1/키패드 누르기.py
@@ -1,4 +1,4 @@
-# 2020 카카오 인턴십 문제
+# # 2020 카카오 인턴십 문제
 
 def solution(numbers, hand):
     answer = ''
@@ -8,8 +8,6 @@ def solution(numbers, hand):
     h = hand.upper()[:1]
 
     for i in numbers:
-        print(L_cur, R_cur)
-        print(answer)
         for j in range(4):
             if i == nums[j][0]:
                 answer = answer + 'L'
@@ -35,5 +33,40 @@ def solution(numbers, hand):
                     else:
                         L_cur = (j,1)
                     break
+
+    return answer
+
+
+# 새로운 풀이법 추가
+def solution(numbers, hand):
+    answer = ''
+    nums = [(3,1),(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+    l,r = (3,0), (3,2)
+
+    for i in numbers:
+        if i%3 == 1:
+            l = nums[i]
+            answer += 'L'
+        elif i%3 == 0 and i != 0:
+            r = nums[i]
+            answer += 'R'
+
+        else:
+            lD = abs(nums[i][0]-l[0]) + abs(nums[i][1]-l[1])
+            rD = abs(nums[i][0]-r[0]) + abs(nums[i][1]-r[1])
+
+            if lD < rD:
+                l = nums[i]
+                answer += 'L'
+            elif lD > rD:
+                r = nums[i]
+                answer += 'R'
+            else:
+                if hand == 'left':
+                    l = nums[i]
+                    answer += 'L'
+                else:
+                    r = nums[i]
+                    answer += 'R'
 
     return answer
